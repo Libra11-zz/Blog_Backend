998a1fdf68b8470420c4b4f186875a9e89538062
Libra <libra_085925@163.com>
2020-05-30 22:43:49 +0800

    add getAllTags api

--- routers/remote-search.js
+++ routers/remote-search.js
@@ -1,11 +1,23 @@
 const koaRouter = require('koa-router')
 const router = new koaRouter()
 const User = require('../DB/models/users')
+const Blog = require('../DB/models/blogs')
 
 //找到所有管理员用户
 const findAllAdminUsers = () => {
   return new Promise((resolve, reject) => {
     User.find({ isAdmin: { $in: true } }, (err, doc) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(doc);
+    });
+  });
+};
+//找到所有文章标签
+const getAllTags = () => {
+  return new Promise((resolve, reject) => {
+    Blog.find({}, { tag: 1 }, (err, doc) => {
       if (err) {
         reject(err);
       }
@@ -26,6 +38,30 @@ router.get('/getAllAdminUsers', async ctx => {
     ctx.body = {
       code: 0,
       info
+    };
+  }
+})
+router.get('/getAllTags', async ctx => {
+  let info = await getAllTags(ctx)
+  if (!info) {
+    ctx.status = 200
+    ctx.body = {
+      code: -1,
+      res: '没有查询到标签'
+    }
+  } else {
+    let res = []
+    for (const item of info) {
+      item.tag.forEach(element => {
+        if (!res.includes(element)) {
+          res.push(element)
+        }
+      });
+    }
+    ctx.status = 200;
+    ctx.body = {
+      code: 0,
+      info: res
     };
   }
 })
